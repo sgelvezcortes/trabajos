1f83d7ef12cc4390846a219f8ee7139c64e5e440
sgelvezcortes <sgelvezcortes@gmail.com>
2025-02-27 11:37:00 -0500

    agrego facto

---
+++ facto.py
@@ -0,0 +1,18 @@
+def fact(n):
+    if n > 0 :
+        fact = 1
+        i = 1
+        while i <= n :
+            fact *= i
+            i = i + 1
+        return fact
+    elif n < 0 :
+        return 0
+    else :
+        return 1
+
+filas = 10
+for n in range(filas):
+    for r in range(n + 1) :
+        print(f"({n}, {r}) = { fact(n) // (fact(r) * fact(n - r)) }  ", end="")
+    print("")

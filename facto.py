eae59de6e909a77b7da970cd4644fe35e6289c6f
sgelvezcortes <sgelvezcortes@gmail.com>
2025-02-27 11:39:26 -0500

    cambie facto

--- facto.py
+++ facto.py
@@ -10,9 +10,10 @@ def fact(n):
         return 0
     else :
         return 1
-
 filas = 10
 for n in range(filas):
     for r in range(n + 1) :
         print(f"({n}, {r}) = { fact(n) // (fact(r) * fact(n - r)) }  ", end="")
     print("")
+    
+print("gatico")
